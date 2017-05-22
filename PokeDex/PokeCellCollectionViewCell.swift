09c2d83eae55b476553be3e210814bc955c9d5c3
Jeff Ripke <jripke74@gmail.com>
2017-05-22 12:49:59 -0700

    add PokeCellCollectionViewCell adn IBOutlet for cell

---
+++ PokeDex/PokeCellCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  PokeDex
+//
+//  Created by Jeff Ripke on 5/22/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
